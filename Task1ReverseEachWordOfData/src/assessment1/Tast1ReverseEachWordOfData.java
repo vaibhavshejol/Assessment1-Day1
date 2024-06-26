16bc3e06f34fb7c0f39f564929a2b14fd05c9dbb
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-26 11:59:06 +0530

    implemented logic of data reading from input file

--- Task1ReverseEachWordOfData/src/assessment1/Tast1ReverseEachWordOfData.java
+++ Task1ReverseEachWordOfData/src/assessment1/Tast1ReverseEachWordOfData.java
@@ -1,14 +1,21 @@
 package assessment1;
 
-import java.io.File;
+import java.io.*;
 
 public class Tast1ReverseEachWordOfData {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Application work properly....");
 		
-		File f=new File("D:\\Assessments\\Assessment1-Day1\\input.txt");
+		FileReader f=new FileReader("D:\\Assessments\\Assessment1-Day1\\input.txt");
+		BufferedReader br=new BufferedReader(f);
 		
+		String data="";
+		String line;
+		while((line=br.readLine())!=null) {
+			data=data+"\n"+line;
+		}
+		System.out.println(data);
 	}
 
 }
