58c547c7c10d1fad0c4b2a354a7464ef99cd6a93
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-26 11:36:35 +0530

    Project folder structure created for task 1

---
+++ Task1ReverseEachWordOfData/src/assessment1/Tast1ReverseEachWordOfData.java
@@ -0,0 +1,14 @@
+package assessment1;
+
+import java.io.File;
+
+public class Tast1ReverseEachWordOfData {
+
+	public static void main(String[] args) {
+		System.out.println("Application work properly....");
+		
+		File f=new File("D:\\Assessments\\Assessment1-Day1\\input.txt");
+		
+	}
+
+}
