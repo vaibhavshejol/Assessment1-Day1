189a3d39f4291418c6a7848df684a8ff4b77ece2
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-26 12:37:29 +0530

    implemented logic of revercing data and writing in output file

--- Task1ReverseEachWordOfData/src/assessment1/Tast1ReverseEachWordOfData.java
+++ Task1ReverseEachWordOfData/src/assessment1/Tast1ReverseEachWordOfData.java
@@ -4,18 +4,52 @@ import java.io.*;
 
 public class Tast1ReverseEachWordOfData {
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("Application work properly....");
-		
-		FileReader f=new FileReader("D:\\Assessments\\Assessment1-Day1\\input.txt");
-		BufferedReader br=new BufferedReader(f);
-		
-		String data="";
-		String line;
-		while((line=br.readLine())!=null) {
-			data=data+"\n"+line;
+	private static String reverseWord(String word) {
+		String symbol = "";
+		// logic to separate symbol and word
+		if (word.endsWith(",") || word.endsWith(".") || word.endsWith("!") || word.endsWith(";")) {
+			symbol = word.substring(word.length() - 1);
+			word = word.substring(0, word.length() - 1);
 		}
-		System.out.println(data);
+
+		// here we reversed the word
+		StringBuilder reversedWord = new StringBuilder(word);
+		String reversed = reversedWord.reverse().toString();
+
+		// here we return that reversed word with concatinating symbol for placing at last
+		return reversed + symbol;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Application is working properly....");
+
+		// here we set the path of input file
+		FileReader f = new FileReader("D:\\Assessments\\Assessment1-Day1\\input.txt");
+		BufferedReader br = new BufferedReader(f);
+
+		// here we set the path where we want to print or write data
+		FileWriter fw = new FileWriter("D:\\Assessments\\Assessment1-Day1\\output.txt", false);
+		BufferedWriter bw = new BufferedWriter(fw);
+
+		// here we declare an empty string for reading data.
+		// and here we read data from file and convert it into character array for the
+		// separating word because it help us to pick up single single word
+		String line;
+		while ((line = br.readLine()) != null) {
+			String[] words = line.split("\\s+");
+
+			StringBuilder reversedLine = new StringBuilder();
+			for (String word : words) {
+				String reversedWord = reverseWord(word);
+				reversedLine.append(reversedWord).append(" ");
+			}
+			// here we write data in file which we reversed
+			bw.write(reversedLine.toString().trim());
+			bw.newLine();
+		}
+		br.close();
+		bw.close();
+
 	}
 
 }
