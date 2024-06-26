01b05a1ce5c46685293a3ec3a7090340656d2355
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-06-26 15:04:13 +0530

    implemented exception handling

--- Task1ReverseEachWordOfData/src/assessment1/Tast1ReverseEachWordOfData.java
+++ Task1ReverseEachWordOfData/src/assessment1/Tast1ReverseEachWordOfData.java
@@ -30,27 +30,32 @@ public class Tast1ReverseEachWordOfData {
 		// here we set the path where we want to print or write data
 		FileWriter fw = new FileWriter("D:\\Assessments\\Assessment1-Day1\\output.txt", false);
 		BufferedWriter bw = new BufferedWriter(fw);
+		
+		try {
+			// here we declare an empty string for reading data.
+			// and here we read data from file and convert it into character array for the
+			// separating word because it help us to pick up single single word
+			String line;
+			System.out.println("\nOutput:\n");
+			while ((line = br.readLine()) != null) {
+				String[] words = line.split("\\s+");
 
-		// here we declare an empty string for reading data.
-		// and here we read data from file and convert it into character array for the
-		// separating word because it help us to pick up single single word
-		String line;
-		System.out.println("\nOutput:\n");
-		while ((line = br.readLine()) != null) {
-			String[] words = line.split("\\s+");
-
-			StringBuilder reversedLine = new StringBuilder();
-			for (String word : words) {
-				String reversedWord = reverseWord(word);
-				reversedLine.append(reversedWord).append(" ");
+				StringBuilder reversedLine = new StringBuilder();
+				for (String word : words) {
+					String reversedWord = reverseWord(word);
+					reversedLine.append(reversedWord).append(" ");
+				}
+				// here we write data in file which we reversed
+				bw.write(reversedLine.toString().trim());
+				bw.newLine();
+				System.out.println(reversedLine);
 			}
-			// here we write data in file which we reversed
-			bw.write(reversedLine.toString().trim());
-			bw.newLine();
-			System.out.println(reversedLine);
+		} catch (Exception ex) {
+			System.out.println("Error is : " + ex);
+		} finally {
+			br.close();
+			bw.close();
 		}
-		br.close();
-		bw.close();
 
 	}
 
